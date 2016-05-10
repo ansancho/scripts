6cb6cc8e739b086cccf0f651ada026c4c708ef03
Angel Sancho <asancho@vhoorl.com>
2016-05-10 18:13:45 +0200

    Version Pre-alfa de recuperacion de empresas tecnologicas desde trabajobasura.info

--- python/Jobs/trabajo_basura.py
+++ python/Jobs/trabajo_basura.py
@@ -9,65 +9,66 @@ from bs4 import BeautifulSoup
 
 def parse_arguments():
     """ Process command line arguments """
-    parser = ArgumentParser(description='Grabs tables from html')
-    parser.add_argument('-u', '--url', help='url to grab from',
-                        required=True)
-    parser.add_argument('-U', '--user', 
+    parser = ArgumentParser(description=
+    		'Grabs full directory of companies from http://trabajobasura.info')    
+    parser.add_argument('-u', '--user', 
     					help='Registered username. Necessary to retrieve the directory',
     					required=True)
-    parser.add_argument('-P', '--passw',
+    parser.add_argument('-p', '--passw',
     				    help='User password',
     				    required=True)    					
     args = parser.parse_args()
     return args
 
 
-def parse_rows(rows):
-    """ Get data from rows """
-    results = []
-    for row in rows:
-        #table_headers = row.find_all('th')
-        #if table_headers:
-        #    results.append([headers.get_text() for headers in table_headers])		
-        table_data = row.find_all('td',attrs = {'class' : ['lin0','lin1']})
-        #table_data = row.find_all('td')
-        if table_data:
-            results.append([data.get_text() for data in table_data])
-    return results
-
-
+def parse_table(table):
+     results = []
+     for i in table.find_all('tr'):
+          tmp = []
+          for j in i.find_all('td',attrs = {'class' : ['lin0','lin1']}):            
+              print j.get_text()        
+          print "****"
+            #tmp.append(j.get_text())
+            #print j.getText(separator='\t',strip=True)          	
+#        results.append("\t".join(tmp))
+     return "\n".join(results)
+     
 def main():
     # Get arguments
     args = parse_arguments()
-    if (args.url) and (args.user) and (args.passw):
-        url = args.url
+    if (args.user) and (args.passw):        
         user = args.user
         passw = args.passw
   
+  	directorio = 'http://www.trabajobasura.info/directorio/'
+    authurl = 'http://www.trabajobasura.info/ingresar.php'
     payload = {
     'action': 'ingresar.php',
     'emailF': user,
     'passwordF': passw }
+    
+    headers = {
+    'User-Agent': 'Mozilla/5.0 (X11; Fedora; Linux x86_64; rv:46.0) Gecko/20100101 Firefox/46.0'    
+	}
 
     sess = session()  
-    sess.post('http://www.trabajobasura.info/ingresar.php', data=payload)
+    sess.post(authurl, data=payload)
+    
     try:   
-          response = sess.get('http://www.trabajobasura.info/directorio/')
-          soup = BeautifulSoup(response.content, 'html.parser' )          
+          response = sess.get(directorio,headers=headers)
+          soup = BeautifulSoup(response.content, 'html.parser' )
           try :
-                 tabla = soup.find('table')
-                
-                 rows = tabla.find_all('tr')
-                 table_data = parse_rows(rows)
-                 for i in table_data:
-                      print '\t'.join(i)
-                       
+                 table = soup.find('table')
+                 
+                 datos = parse_table(table)
+                 print datos
+                                        
           except AttributeError as e:
                  print 'No tables found, exiting'
                  sys.exit(1)
 
     except Exception as e:
-   		  print 'An error occured fetching %s \n %s' % (url, str(e))
+   		  print 'An error occured fetching trabajobasura table \n %s' % str(e)
    		  sys.exit(1)
    		  
 if __name__ == '__main__':
