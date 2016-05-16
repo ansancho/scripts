7d9589ab94354ad043812e10ca769c2fffccefd0
Angel Sancho <asancho@vhoorl.com>
2016-05-16 15:07:37 +0200

    I have a half working script. It retrives correctly the list of the companies and sort them by rating

--- python/Jobs/trabajo_basura.py
+++ python/Jobs/trabajo_basura.py
@@ -7,6 +7,7 @@ from argparse import ArgumentParser
 from bs4 import BeautifulSoup
 from time import sleep
 import logging
+import csv
 
 '''
 Global variables
@@ -33,27 +34,49 @@ def parse_arguments():
     parser.add_argument('-p', '--passw',
     				    help='User password',
     				    required=True)
-    parser.add_argument('-d','--debug',                        
+    parser.add_argument('-d','--debug', nargs='?',
     					help='Print debug messages, write something like true or True',
     					required=False)
+    parser.add_argument('-s','--sort',
+                        required=False)
     args = parser.parse_args()
     return args
 
 
 def parse_table(table):
-     results = []
-     for i in table.find_all('tr'):
-          tmp = []
-          for j in i.find_all('td',attrs = {'class' : ['lin0','lin1']}):            
-              print j.get_text()        
-          print "****"
-     return "\n".join(results)
+    logger = logging.getLogger(__name__)
+    results = []
+    company = []
+    for i in table.find_all('tr'):
+            tmp = i.find_all('td')
+            if tmp:
+                #nombre de la empresa y enlace interno                
+                a = tmp[0].find_all('a',href=True)
+                for j in a:
+                    company.append(j.getText().encode('utf-8'))
+                    company.append(directorio+j['href'].encode('utf-8'))
+                #votos
+                company.append(float(tmp[1].getText()))
+                #numero de comentarios - popularidad
+                company.append(float(tmp[2].getText()))
+                #Enlace externo
+                a = tmp[3].find_all('a',href=True)
+                if a:
+                    for j in a:                    
+                        company.append("http:"+j['href'].encode('utf-8'))
+                else:
+                    company.append("")
+                results.append(company)                
+                company = []
+    return results           
      
 def main():
     # Get arguments
     args = parse_arguments()
     logging.basicConfig()
     logger = logging.getLogger(__name__)
+    fichero = open('junkcs.csv','w')
+    excel   = csv.writer(fichero)
     if (args.user) and (args.passw):        
         params['emailF']= args.user
         params['passwordF'] = args.passw
@@ -70,30 +93,29 @@ def main():
         response = sess.get(directorio,headers=headers)
         soup = BeautifulSoup(response.content,'html.parser')
         ''' The index of directory pages are in the font tags '''
-        index = len(soup.find_all('table')[0].find_all('font'))
-        logger.debug(index)
+        index = len(soup.find_all('table')[0].find_all('font'))        
         
         tables = ""
-        for i in range (1,3):
+        for i in range (1,index+1):
             logger.info("Procesando " + directorio+ "?pag=" + str(i))
             response = sess.get(directorio+ "?pag=" + str(i),headers=headers)
             soup = BeautifulSoup(response.content,'html.parser')
             tables += str(soup.find_all('table')[-1])
             ''' We do not want to make DDOS '''
-            sleep (0.5)
-        soupTables=BeautifulSoup(tables,'html.parser')
-      
-        '''		
-            for i in range(2,indice):
-			response = sess.get(directorio+ "?pag=" + str(i),headers=headers)
-			soup = BeautifulSoup(response.content,'html.parser')
-        '''
-
+            sleep (0.3)        
+        listado = parse_table(BeautifulSoup(tables,'html.parser'))
+        #ordenar por puntuacion
+        listado.sort(key=lambda x: x[2], reverse=True)
+        excel.writerows(listado)
+        fichero.close()
+        sys.exit(1)
+        
+        
     except RequestException as sessionError:
-        logger.info('An error occured fetching trabajobasura.info page \n %s\n' % str(sessionError))
+        logger.debug('An error occured fetching trabajobasura.info page \n %s\n' % str(sessionError))
         sys.exit(1)
     except Exception as e:
-        logger.info( 'An error occured\n %s\n' % str(e),exc_info=True)
+        logger.debug( 'An error occured\n %s\n' % str(e),exc_info=True)
         sys.exit(1)
 
 if __name__ == '__main__':
